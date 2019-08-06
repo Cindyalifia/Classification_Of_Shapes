baa654040b872328c40de5c95dafb49168ed5422
Cindy Alifia <cindyalifia39@gmail.com>
2019-08-07 00:47:38 +0700

    adding some codes

--- README.md
+++ README.md
@@ -4,8 +4,36 @@
 
 ### Tools that needed to be installed are :
 - Keras version 2.2.4
-- Tensorflow version 1.13.0-rc2
-- Pandas version 0.23.4
+- matplotlib 
+
+### Introduction
+In this project, I make some classification to classify shape there are circle, square, and triangle. I use keras library and some other library and there are something you have to do before running the program.
+
+### Installation 
+- First step, you have to install keras on your pc   
+    you can watch keras installation on https://keras.io/#installation
+
+- Second step, you have to install matplotlib on your pc   
+    $ pip install matplotlib
+  
 
 ### Dataset
-In this project, I use shapes for dataset which is circle, square, and triangle. I've got this data from https://www.kaggle.com/cactus3/basicshapes. There are 300 image containing 100 pictures each of circle, rectangle, and triangle. I split data for training set 67% and for test set 33%.
+In this project, I use shapes for dataset which is circle, square, and triangle. I've got this data from https://www.kaggle.com/cactus3/basicshapes. There are 300 image containing 100 pictures each of circle, rectangle, and triangle. I split data for training set 67% and for test set 33%. 
+
+### Result 
+After I train my program, I got the accuracy is 92.04% using 20 epoch.
+
+![](./SS/train.png)
+
+And also I plot my training result :
+
+![](./SS/model_acc.png)
+
+If we look closer to my image, we can see that the accuracy from my train is almost overfitting. and that's why I make 20 epochs.  
+And the result for loss is :
+
+![](./SS/model_loss.png)
+
+### My Model Result
+I save my model on 'my_model.h5'.
+
--- shapeClassification.py
+++ shapeClassification.py
@@ -1,47 +1,70 @@
 
-# importing the keras libraries and packages
-from keras.models import Sequential 
+from keras.models import load_model
+import keras
+from keras.models import Sequential
 from keras.layers import Convolution2D
 from keras.layers import MaxPooling2D
-from keras.layers import Flatten 
+from keras.layers import Flatten
 from keras.layers import Dense
+from keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
 
-# initialising the CNN
+# Initialising the CNN
 classifier = Sequential()
-classifier.add(Convolution2D(32, 3, 3, input_shape = (64, 64, 3), activation = 'relu')) 
-classifier.add(MaxPooling2D(pool_size = (2, 2)))
+classifier.add(Convolution2D(
+    16, 3, 3, input_shape=(28, 28, 3), activation='relu'))
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
+classifier.add(Convolution2D(32, 3, 3, activation='relu'))
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
 classifier.add(Flatten())
-classifier.add(Dense(output_dim = 128, activation = 'relu'))
-classifier.add(Dense(output_dim = 8, activation = 'relu'))
-classifier.add(Dense(output_dim = 1, activation = 'sigmoid'))
-classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+classifier.add(Dense(56, activation='relu', kernel_initializer='uniform'))
+classifier.add(Dense(3, activation='softmax', kernel_initializer='uniform'))
 
-# Part 2 - Fitting the CNN to the images 
-from keras.preprocessing.image import ImageDataGenerator 
+# Compiling the CNN
+classifier.compile(
+    optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
+
+# Image Preprocessing
 train_datagen = ImageDataGenerator(
-        rescale = 1./255,
-        shear_range = 0.2,
-        zoom_range = 0.2,
-        horizontal_flip = True)
+    rescale=1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
 
-test_datagen = ImageDataGenerator(rescale=1./255)
+test_datagen = ImageDataGenerator(rescale=1. / 255)
 
 training_set = train_datagen.flow_from_directory('training_set',
-                                                 target_size = (64, 64),
-                                                 batch_size = 32, 
-                                                 class_mode = 'binary')
-   
+                                                 target_size=(28, 28),
+                                                 batch_size=1,
+                                                 class_mode='categorical')
+
 test_set = test_datagen.flow_from_directory('test_set',
-                                            target_size = (64, 64),
-                                            batch_size = 32, 
-                                            class_mode = 'binary')
+                                            target_size=(28, 28),
+                                            batch_size=1,
+                                            class_mode='categorical')
 
-classifier.fit_generator(training_set, 
-                         samples_per_epoch = 35, 
-                         nb_epoch = 10,
-                         validation_data = test_set,
-                         nb_val_samples = 2000)
+history = classifier.fit_generator(training_set,
+                                   steps_per_epoch=len(training_set.filenames),
+                                   epochs=20,
+                                   validation_data=test_set,
+                                   validation_steps=len(test_set.filenames))
+classifier.save('my_model.h5')  # creates a HDF5 file 'my_model.h5'
 
+# Plot training & validation accuracy values
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('Model accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.show()
 
-
+# Plot training & validation loss values
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.show()
